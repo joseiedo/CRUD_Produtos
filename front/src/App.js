4b7b00f8b09688a318c780712e9a7fcfec295bc0
jose iedo <iedoduarte1@gmail.com>
2022-10-02 22:04:47 -0300

    resolvido bug no css dos inputs do formulário

--- front/src/App.js
+++ front/src/App.js
@@ -5,12 +5,9 @@ import React from "react";
 import Menu from "./Menu";
 import Produto from "./Produto";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-import Button from 'react-bootstrap/Button';
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const App = () => {
   /**
@@ -32,49 +29,65 @@ const App = () => {
    * localhost:8080/produtos/remover
    */
 
-  const [dados, setDados] = React.useState(null)
-  const [produto, setProduto] = React.useState(null)
+  const [dados, setDados] = React.useState(null);
+  const [produto, setProduto] = React.useState(null);
 
   function atualizarDadosProdutos() {
     fetch("http://localhost:8080/produtos")
       .then((r) => r.json())
-      .then((r) => { setDados(r); })
+      .then((r) => {
+        setDados(r);
+      });
   }
   React.useEffect(() => {
     atualizarDadosProdutos();
   }, []);
 
   return (
-    <div style={{
-      minHeight: "100vh"
-    }}
+    <div
+      style={{
+        minHeight: "100vh",
+      }}
       className="d-flex p-4 justify-content-center align-items-center bg-dark"
     >
       <div
-
-        style={{ minHeight: "80vh", maxWidth: "700px", backgroundColor: "#fff" }}
+        style={{
+          maxWidth: "700px",
+          backgroundColor: "#fff",
+        }}
         className="shadow d-flex flex-column p-4 rounded-3 justify-content-center align-items-center container-fluid"
       >
         <h1>Cadastro de produtos</h1>
         <Menu setProduto={setProduto} />
 
-        {dados ? <Container fluid className="">
-          <Row className=" justify-content-n-center">
-            {dados.map((produto) => {
-              return (<Col key={produto.id} className="mb-4">
-                <Produto dados={produto} />
-              </Col>
-              )
-            })}
-            {produto && <Col><Produto dados={produto} /></Col>}
-          </Row>
-        </Container> :
+        {dados ? (
+          <Container
+            fluid
+            className=""
+            style={{ overflowY: "scroll", height: "600px" }}
+          >
+            <Row className=" justify-content-n-center">
+              {dados.map((produto) => {
+                return (
+                  <Col key={produto.id} className="mb-4">
+                    <Produto dados={produto} />
+                  </Col>
+                );
+              })}
+              {produto && (
+                <Col>
+                  <Produto dados={produto} />
+                </Col>
+              )}
+            </Row>
+          </Container>
+        ) : (
           <div className="d-flex align-items-center" style={{ height: "60vh" }}>
             <h3>nenhum produto adicionado...</h3>
           </div>
-        }
+        )}
       </div>
-    </div >
+    </div>
   );
 };
 
--- front/src/Formulario.js
+++ front/src/Formulario.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const Formulario = ({ handleClose }) => {
   const [nome, setNome] = React.useState("");
@@ -17,73 +16,102 @@ const Formulario = ({ handleClose }) => {
     } else {
       setValidacao(false);
     }
-  }, [nome, categoria, imagem, descricao])
+  }, [nome, categoria, imagem, descricao]);
 
   return (
-    <Row className='g-2'>
-      <Col md>
+    <>
+      <Col>
         <Form.Group className="mb-3">
           <Form.Label>Nome do produto</Form.Label>
-          <Form.Control required type="text" placeholder="Ex: Notebook Samsung" value={nome} onChange={({ target }) => {
-            setNome(target.value);
-          }} />
+          <Form.Control
+            required
+            type="text"
+            placeholder="Ex: Notebook Samsung"
+            value={nome}
+            onChange={({ target }) => {
+              setNome(target.value);
+            }}
+          />
         </Form.Group>
-      </Col>
 
-      <Col md>
         <Form.Group className="mb-3">
           <Form.Label>Categoria</Form.Label>
-          <Form.Select required value={categoria} onChange={({ target }) => setCategoria(target.value)}>
-            <option value='default' disabled >Selecione uma categoria</option>
-            <option value='Celulares e Smartphones'>Celulares e Smartphones</option>
-            <option value='Computadores e Notebooks'>Computadores e Notebooks</option>
+          <Form.Select
+            required
+            value={categoria}
+            onChange={({ target }) => setCategoria(target.value)}
+          >
+            <option value="default" disabled>
+              Selecione uma categoria
+            </option>
+            <option value="Celulares e Smartphones">
+              Celulares e Smartphones
+            </option>
+            <option value="Computadores e Notebooks">
+              Computadores e Notebooks
+            </option>
             <option value="Moda">Moda</option>
-            <option value="Saúde e Qualidade de vida">Saúde e Qualidade de vida</option>
+            <option value="Saúde e Qualidade de vida">
+              Saúde e Qualidade de vida
+            </option>
             <option value="Instrumentos musicais">Instrumentos musicais</option>
             <option value="Eletrônicos">Eletrônicos</option>
             <option value="Outro">Outro</option>
           </Form.Select>
         </Form.Group>
       </Col>
-
-      <Col xl>
-        <Form.Group className="mb-3">
-          <Form.Label>Imagem</Form.Label>
-          <Form.Control required type="text" placeholder='Insira a URL da imagem' value={imagem} onChange={({ target }) => {
+      <Form.Group className="mb-3">
+        <Form.Label>Imagem</Form.Label>
+        <Form.Control
+          required
+          type="text"
+          placeholder="Insira a URL da imagem"
+          value={imagem}
+          onChange={({ target }) => {
             setImagem(target.value);
-          }} />
-        </Form.Group>
-      </Col>
-      <Col xl className="mb-3">
+          }}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
         <Form.Label>Descrição</Form.Label>
-        <Form.Control required as="textarea" rows={3} maxLength={250} placeholder="Máximo de 250 caracteres" value={descricao} onChange={({ target }) => {
-          setDescricao(target.value);
-        }} />
-      </Col>
+        <Form.Control
+          required
+          as="textarea"
+          rows={3}
+          maxLength={250}
+          placeholder="Máximo de 250 caracteres"
+          value={descricao}
+          onChange={({ target }) => {
+            setDescricao(target.value);
+          }}
+        />
+      </Form.Group>
+      <Button
+        onClick={(e) => {
+          e.preventDefault();
+          handleClose(true);
 
-      <Button onClick={(e) => {
-        e.preventDefault();
-        handleClose(true);
-
-        fetch("http://localhost:8080/cadastro", {
-          method: "POST",
-          body: JSON.stringify({
-            nome: nome,
-            categoria: categoria,
-            imagem: imagem,
-            descricao: descricao
-          }),
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          }
-
-        })
-
-      }} variant="primary" type="submit" disabled={validacao}>
+          fetch("http://localhost:8080/cadastro", {
+            method: "POST",
+            body: JSON.stringify({
+              nome: nome,
+              categoria: categoria,
+              imagem: imagem,
+              descricao: descricao,
+            }),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+          }).then((r) => console.log(r));
+        }}
+        variant="primary"
+        type="submit"
+        disabled={validacao}
+      >
         Cadastrar
       </Button>
-    </Row >
-  )
-}
+    </>
+  );
+};
 
-export default Formulario
+export default Formulario;
